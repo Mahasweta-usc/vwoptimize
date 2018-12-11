ca6d6be0d5260b8ee2e6f0270cee2702925c31cb
Mahasweta Chakraborti <mahaswec@usc.edu>
2018-12-10 16:13:43 -0800

    Update vwoptimize.py

--- vwoptimize.py
+++ vwoptimize.py
@@ -455,7 +455,7 @@ def run_subprocesses(cmds, workers=None, importance=None):
 
                 if popen.stdout is not None or popen.stderr is not None:
                     out, err = popen.communicate()
-                    print(out.decode('utf-8'))
+                    print(err.decode('utf-8'))
                     out = (out or '') + (err or '')
                     outputs.setdefault(popen._name, []).append(out)
 
